5cdaca4deb6e353bd98d0259cbbc479c1412eef5
prkhr-agrwl <aprakhar51@gmai.com>
2019-06-28 16:27:21 +0530

    first commit

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,36 @@
+import "./SeasonDisplay.css";
+import React from "react";
+
+const seasonConfig = {
+	summer: {
+		text: "Let's go to the fucking beach.",
+		iconName: "sun"
+	},
+	winter: {
+		text: "Its fucking Chilly out there",
+		iconName: "snowflake"
+	}
+};
+
+const getSeason = (lat, month) => {
+	if (month > 2 && month < 9) {
+		return lat > 0 ? "summer" : "winter";
+	} else {
+		return lat > 0 ? "winter" : "summer";
+	}
+};
+
+const SeasonDisplay = function(props) {
+	var month = new Date().getMonth();
+	const season = getSeason(props.lat, month);
+	const { text, iconName } = seasonConfig[season];
+	return (
+		<div className={`season-display ${season}`}>
+			<i className={`icon-left massive ${iconName} icon`} />
+			<h1>{text}</h1>
+			<i className={`icon-right massive ${iconName} icon`} />
+		</div>
+	);
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./spinner";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+	state = { lat: null, errMessage: "" };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			position => this.setState({ lat: position.coords.latitude }),
+			err => this.setState({ errMessage: err.message })
+		);
+	}
+
+	render() {
+		if (this.state.errMessage && !this.state.lat) {
+			return <div>Error: {this.state.errMessage}</div>;
+		} else if (!this.state.errMessage && this.state.lat) {
+			return <SeasonDisplay lat={this.state.lat} />;
+		}
+		return <Spinner message="Tell us where you are." />;
+	}
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
